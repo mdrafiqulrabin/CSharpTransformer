5062fed91d037e692c41906c7a2434ac93965ca2
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-11-01 03:26:35 -0500

    PermuteStatement: Apply to one.

--- src/PermuteStatement.cs
+++ src/PermuteStatement.cs
@@ -19,36 +19,59 @@ namespace CSharpTransformer.src
             CompilationUnitSyntax root = Common.GetParseUnit(csFile);
             if (root != null)
             {
-                root = ApplyPermuteStatement(root);
-                Common.SaveTransformation(root, csFile);
+                // apply to single place
+                var stmtNodes = root.DescendantNodes().OfType<StatementSyntax>().ToList();
+                for (int place = 0; place < stmtNodes.Count - 1; place++)
+                {
+                    var modRoot = ApplyToOne(Common.GetParseUnit(csFile), place);
+                    Common.SaveTransformation(modRoot, csFile, Convert.ToString(place + 1));
+                }
+
+                // apply to all place 
+                root = ApplyToAll(root);
+                Common.SaveTransformation(root, csFile, Convert.ToString(0));
             }
         }
 
-        public CompilationUnitSyntax ApplyPermuteStatement(CompilationUnitSyntax root)
+        public CompilationUnitSyntax ApplyToOne(CompilationUnitSyntax root, int p)
+        {
+            var statements = root.DescendantNodes().OfType<StatementSyntax>().ToList();
+            if (PermeableStatement(statements[p], statements[p+1]))
+            {
+                root = root.ReplaceNodes(new[] { statements[p], statements[p+1] },
+                    (original, _) => original == statements[p] ? statements[p+1] : statements[p]);
+            }
+            return root;
+        }
+
+        public CompilationUnitSyntax ApplyToAll(CompilationUnitSyntax root)
         {
             var statements = root.DescendantNodes().OfType<StatementSyntax>().ToList();
             for (int i = 0, j = 1; i < statements.Count - 1; i++, j++)
             {
-                if (statements[i].Parent == statements[j].Parent
-                    && !(NotPermeableStatement(statements[i]) || NotPermeableStatement(statements[j])))
+                if (PermeableStatement(statements[i], statements[j]))
                 {
-                    var iIdentifiers = statements[i].DescendantTokens().Where(x => x.IsKind(SyntaxKind.IdentifierToken)).Select(x => x.ToString()).ToList();
-                    var jIdentifiers = statements[j].DescendantTokens().Where(x => x.IsKind(SyntaxKind.IdentifierToken)).Select(x => x.ToString()).ToList();
-                    if (!iIdentifiers.Intersect(jIdentifiers).Any() && !jIdentifiers.Intersect(iIdentifiers).Any())
-                    {
-                        /*StatementSyntax tmpSt = SyntaxFactory.ParseStatement("int mrabin = 0;");
-                        root = root.ReplaceNode(statements[i], tmpSt);
-                        root = root.ReplaceNode(statements[j], iStatement);
-                        root = root.ReplaceNode(tmpSt, jStatement);*/
-
-                        root = root.ReplaceNodes(new[] { statements[i], statements[j] },
-                            (original, _) => original == statements[i] ? statements[j] : statements[i]);
-                        statements = root.DescendantNodes().OfType<StatementSyntax>().ToList();
-                        i++; j++;
-                    }
+                    root = root.ReplaceNodes(new[] { statements[i], statements[j] },
+                        (original, _) => original == statements[i] ? statements[j] : statements[i]);
+                    statements = root.DescendantNodes().OfType<StatementSyntax>().ToList();
                 }
             }
             return root;
+        }
+
+        private bool PermeableStatement(StatementSyntax stmti, StatementSyntax stmtj)
+        {
+            if (stmti.Parent == stmtj.Parent
+                && !(NotPermeableStatement(stmti) || NotPermeableStatement(stmtj)))
+            {
+                var iIdentifiers = stmti.DescendantTokens().Where(x => x.IsKind(SyntaxKind.IdentifierToken)).Select(x => x.ToString()).ToList();
+                var jIdentifiers = stmtj.DescendantTokens().Where(x => x.IsKind(SyntaxKind.IdentifierToken)).Select(x => x.ToString()).ToList();
+                if (!iIdentifiers.Intersect(jIdentifiers).Any() && !jIdentifiers.Intersect(iIdentifiers).Any())
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         private bool NotPermeableStatement(StatementSyntax node)
